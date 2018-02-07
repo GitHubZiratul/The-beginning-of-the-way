1b27e04139c98ebe668cd415f7d722a33db385f9
Ziratul <35899614+GitHubZiratul@users.noreply.github.com>
2018-02-07 16:55:19 +0300

    Add files via upload

---
+++ i/chaos.py
@@ -0,0 +1,43 @@
+# # squad=input('Введите состав подразделения через запятую:')
+# # squad= squad.split(', ')
+# # print (squad)
+
+# # list_of_man= ['Кто-то_1', 'Кто-то_2', 'Кто-то_3']
+# # list_of_man= ' Рак '.join(list_of_man)
+# # print(list_of_man)
+
+
+# # name=input('Ваш позывной ,дерьма кусок!!!')
+# # if name in squad:
+# #   print ('В дисбат')
+# # else:
+# #   print ('Ты не туда попал ,мальчик') 
+# # k=1
+# # for i in squad:
+# #   print (str(k)+i,'<3')
+# #   k+=1
+# # name=input('Кого ищем')
+# # for i in squad:
+# #     if i==name:
+# #         print('Иди программировать')
+# #         break
+# # else:
+# #     print ('Он в самоволке')
+
+
+# ammo_dict={
+#     'Russia': ['АК', 'Lobarev', 'Винтарь'],
+#     'USA': ['Deasert Eagle', 'M16', 'Barret'],
+#     'Germany':['Heckler& Koch']}
+# for i in ammo_dict.values():
+#     print(i)
+
+# print()
+# for i in ammo_dict.keys():
+#     print(i)
+# print()
+# for i in ammo_dict.items():
+#     print (i)
+# print()
+for i in range (100,0,-10):
+    print (i)
